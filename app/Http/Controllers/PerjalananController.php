b41105edbc81378e6bdcbde567e795e8b14770a6
arbim26 <sangserketa@gmail.com>
2022-09-21 09:58:51 +0700

    Auto insert Id Auth

--- app/Http/Controllers/PerjalananController.php
+++ app/Http/Controllers/PerjalananController.php
@@ -13,35 +13,37 @@ class PerjalananController extends Controller
     public function perjalanan()
     { 
         $user = User::find(Auth::user()->id);
+        // dd($user);
         $data = perjalanan::where('user_id', $user->id)->get();
         return view('perjalanan.perjalanan', compact('data'));
     }
 
-    // public function create(Request $request)
-    // {
-    //     $user = User::find(Auth::user()->id);
-    //     // dd($user);
-    //     // $this->validate($request, [
-    //     //     'tanggal' => 'required',
-    //     //     'waktu'=> 'required',
-    //     //     'lokasi'=> 'required',
-    //     //     'suhu'=> 'required',
-    //     // ]);
-    //     perjalanan::create([
-    //         'user_id' =>$user->id,
-    //         'tanggal' =>$request->tanggal,
-    //         'waktu' =>$request->waktu,
-    //         'lokasi' =>$request->lokasi,
-    //         'suhu' =>$request->suhu,
-    //     ]);
-    //     return redirect()->route('perjalanan');
+    public function create(Request $request)
+    {
+        // $this->validate($request, [
+        //         'tanggal' => 'required',
+        //         'waktu'=> 'required',
+        //         'lokasi'=> 'required',
+        //         'suhu'=> 'required',
+        //     ]);
+        // $user = User::find(Auth::user());
+        $user = Auth::user();
+        // dd($user);
+        perjalanan::create([
+            'user_id' =>$user->id,
+            'tanggal' =>$request->tanggal,
+            'waktu' =>$request->waktu,
+            'lokasi' =>$request->lokasi,
+            'suhu' =>$request->suhu,
+        ]);
+        return redirect()->route('perjalanan');
+    }
+
+
+    // public function create(Request $request){
+    //     // dd($request);
+    //     Perjalanan::create($request->all());
+    //     return redirect(route('perjalanan'))->with('message','Sending infomation successfully');
     // }
 
-
-    public function create(Request $request){
-        // dd($request);
-        Perjalanan::create($request->all());
-        return redirect(route('perjalanan'))->with('message','Sending infomation successfully');
-    }
-    
 }
